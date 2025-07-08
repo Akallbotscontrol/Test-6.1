2b2c7d4b811ec7d9be0d825bd4f7fbac66870320
Ak all bots Control <akallbotscontrol@gmail.com>
2025-07-08 21:45:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ API_ID = int(os.environ.get("API_ID", "22289037"))
 API_HASH = os.environ.get("API_HASH", "ced5a994db436661f9af63c4b5247ac6")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7641363143:AAGPOTTRqtC0a0bE2tcL91vjGECpE0sgxe0")
 SUPPORT = os.environ.get("SUPPORT", "https://t.me/RMCBACKUP")
-            
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://RMCFILTERBOT:RMCFILTERBOT@rmcfilterbot.bm5g1.mongodb.net/?retryWrites=true&w=majority&appName=RMCFILTERBOT")
-
-ADMIN = int(os.environ.get("ADMIN", "7037505654"))  # your Telegram user ID
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002273915928"))  # log group/channel ID
+ADMIN = int(os.environ.get("ADMIN", "7037505654"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002273915928"))
