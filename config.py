fdbb6412527c60c88c0b5ede5a784b5a5b9ae403
Ak all bots Control <akallbotscontrol@gmail.com>
2025-07-08 22:01:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "22289037"))
 API_HASH = os.environ.get("API_HASH", "ced5a994db436661f9af63c4b5247ac6")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7641363143:AAGPOTTRqtC0a0bE2tcL91vjGECpE0sgxe0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7490778826:AAHQocs5ICH4SsMdBfvbS1eAB52FnEajCbM")
 SUPPORT = os.environ.get("SUPPORT", "https://t.me/RMCBACKUP")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://RMCFILTERBOT:RMCFILTERBOT@rmcfilterbot.bm5g1.mongodb.net/?retryWrites=true&w=majority&appName=RMCFILTERBOT")
 ADMIN = int(os.environ.get("ADMIN", "7037505654"))
