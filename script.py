dcfd5f3d1e4bd170352c7c3a42c5b2096516687a
Ak all bots Control <akallbotscontrol@gmail.com>
2025-07-09 17:51:23 +0530

    Delete script.py

--- script.py
+++
@@ -1,9 +0,0 @@
-from script import spell1, spell2, get_mode, set_mode
-
-# Toggle checker
-if spell1.get(user_id, True):  # Enabled by default
-    # do spell check
-    pass
-
-# Change search mode
-set_mode(chat_id, "inline")
